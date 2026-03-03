8e3d344614d74fca66ab3f0569feb830440c6ad2
Yin Po Po Aung <70101866+yppaung@users.noreply.github.com>
2026-03-02 23:38:40 -0600

    Implement Power

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,6 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
+		return Math.Pow(double.Parse(x), double.Parse(y));
 	}
 }
